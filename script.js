89e8f4ebbd6fc6f0d396d7d9944bffeffd396c39
Diego Flausino <diegocflausino@gmail.com>
2024-07-30 19:32:45 -0300

    commit responsividade do cabeçalho e da section perfil

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
     //Seleção de Elementos
+    const navProjetos = document.getElementById('nav-projetos')
+    const navFormacao = document.getElementById('nav-formacao')
+    const navContatos = document.getElementById('nav-contatos')
     const inicio = document.getElementById('inicio')
     
+    navFormacao.addEventListener('click', function() {
+        const tituloFormacao = document.getElementById('formacao')
+        if (tituloFormacao) {
+            tituloFormacao.scrollIntoView({ behavior: 'smooth' })
+        }
+    })
+    
+    navContatos.addEventListener('click', function() {
+        const tituloContatos = document.getElementById('contatos')
+        if (tituloContatos) {
+            tituloContatos.scrollIntoView({ behavior: 'smooth' })
+        }
+    })
+    
+    navProjetos.addEventListener('click', function(){
+        const tituloProjeto = document.getElementById("projetos")
+        if (tituloProjeto) {
+            tituloProjeto.scrollIntoView({ behavior: 'smooth' })
+        }
+    })
 
     inicio.addEventListener('click', function(){
         const perfil = document.getElementById('cabecalho')
